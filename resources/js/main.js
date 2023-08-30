c9e08e39fd56a609243e1ee410b2bb134ff0d976
rafi967 <raafiihaque@gmail.com>
2023-08-30 21:45:32 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+// MIXITUP
+var mixer = mixitup('.work-grid');
+
+// NAVIGATION
+function openMenu(){
+    document.getElementById("menu-container").style.width = "100%";
+}
+
+function closeMenu(){
+    document.getElementById("menu-container").style.width = "0";
+}
